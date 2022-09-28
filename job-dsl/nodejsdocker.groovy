152f4cfa2fbee1e4b2fe973d6ba8ab074202ad88
Kelvin <70008108+ktitong02@users.noreply.github.com>
2022-09-28 10:51:30 +0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('ktitong2/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('docker-hub')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
